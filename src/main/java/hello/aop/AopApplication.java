4b8a8a687a7163ddb79087ba896c025fa14822bc
yn15@direa.co.kr <yn15@naver.com>
2024-12-09 16:02:15 +0900

    Create: AOP 구현 - 어드바이스 추가 및 포인트컷 추가 적용

--- src/main/java/hello/aop/AopApplication.java
+++ src/main/java/hello/aop/AopApplication.java
@@ -2,13 +2,15 @@ package hello.aop;
 
 import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 //@Import(AspectV1.class)
-@Import(AspectV2.class)
+//@Import(AspectV2.class)
+@Import(AspectV3.class)
 public class AopApplication {
 
 	public static void main(String[] args) {
