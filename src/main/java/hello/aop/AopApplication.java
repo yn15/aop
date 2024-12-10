2c40c93f802938f6a2d35d4560499d236ae7922c
yn15 <yn15@naver.com>
2024-12-10 15:15:23 +0900

    Create: AOP 구현 - 어드바이스의 종류

--- src/main/java/hello/aop/AopApplication.java
+++ src/main/java/hello/aop/AopApplication.java
@@ -1,8 +1,6 @@
 package hello.aop;
 
-import hello.aop.order.aop.AspectV1;
-import hello.aop.order.aop.AspectV2;
-import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -10,7 +8,10 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
+//@Import(AspectV3.class)
+//@Import(AspectV4Pointcut.class)
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@Import(AspectV6Advice.class)
 public class AopApplication {
 
 	public static void main(String[] args) {
